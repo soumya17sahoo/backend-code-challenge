14e9b88c6107bcbd1c3a7fbb04fe735563d07327
Soumyaranjan Sahoo <soumya17.sahoo@gmail.com>
2026-01-06 00:43:13 +0530

    All Task have been completed. Soumya Ranjan Sahoo

--- CodeChallenge.Api/Controllers/MessagesController.cs
+++ CodeChallenge.Api/Controllers/MessagesController.cs
@@ -1,3 +1,4 @@
+using CodeChallenge.Api.Logic;
 using CodeChallenge.Api.Models;
 using CodeChallenge.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -10,45 +11,90 @@ public class MessagesController : ControllerBase
 {
     private readonly IMessageRepository _repository;
     private readonly ILogger<MessagesController> _logger;
+    private readonly IMessageLogic _messageLogic;
 
-    public MessagesController(IMessageRepository repository, ILogger<MessagesController> logger)
+    public MessagesController(IMessageRepository repository, ILogger<MessagesController> logger, IMessageLogic messageLogic)
     {
         _repository = repository;
         _logger = logger;
+        _messageLogic = messageLogic;
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Message>>> GetAll(Guid organizationId)
     {
-        // TODO: Implement
-        throw new NotImplementedException();
+        var messages = await _messageLogic.GetAllMessagesAsync(organizationId);
+        return Ok(messages);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Message>> GetById(Guid organizationId, Guid id)
     {
-        // TODO: Implement
-        throw new NotImplementedException();
+        var message = await _messageLogic.GetMessageAsync(organizationId, id);
+
+        if (message == null)
+            return NotFound();
+
+        return Ok(message);
     }
 
     [HttpPost]
     public async Task<ActionResult<Message>> Create(Guid organizationId, [FromBody] CreateMessageRequest request)
     {
-        // TODO: Implement
-        throw new NotImplementedException();
+        var result = await _messageLogic.CreateMessageAsync(organizationId, request);
+
+        return result switch
+        {
+            Created<Message> created =>
+                CreatedAtAction(
+                    nameof(GetById),
+                    new { organizationId, id = created.Value.Id },
+                    created.Value),
+
+            Conflict conflict =>
+                Conflict(conflict.Message),
+
+            ValidationError validation =>
+                BadRequest(validation.Errors),
+
+            _ => StatusCode(StatusCodes.Status500InternalServerError)
+        };
     }
 
     [HttpPut("{id}")]
     public async Task<ActionResult> Update(Guid organizationId, Guid id, [FromBody] UpdateMessageRequest request)
     {
-        // TODO: Implement
-        throw new NotImplementedException();
+        var result = await _messageLogic.UpdateMessageAsync(organizationId, id, request);
+
+        return result switch
+        {
+            Updated =>
+                NoContent(),
+
+            NotFound notFound =>
+                NotFound(notFound.Message),
+
+            ValidationError validation =>
+                BadRequest(validation.Errors),
+
+            _ => StatusCode(StatusCodes.Status500InternalServerError)
+        };
     }
 
     [HttpDelete("{id}")]
     public async Task<ActionResult> Delete(Guid organizationId, Guid id)
     {
-        // TODO: Implement
-        throw new NotImplementedException();
+        var result = await _messageLogic.DeleteMessageAsync(organizationId, id);
+
+        return result switch
+        {
+            Deleted =>
+                NoContent(),
+
+            NotFound notFound =>
+                NotFound(notFound.Message),
+
+            _ => StatusCode(StatusCodes.Status500InternalServerError)
+        };
     }
 }
